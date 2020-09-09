7a49af5d48ad86e116502d7ff2d76dcc4e5c1b00
ksh24865 <8819889@naver.com>
2020-09-09 22:46:32 +0900

    change_code_structure

---
+++ conf.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+TODAY = datetime.today().strftime("%Y%m%d")
+
+COVID_KEY_START = '?serviceKey=MMq1VsRlz5qKvsdKDrvMavJB5rGdJOA8JGKgyojceXcL5tj6MJtzG21jN30ke9OOHZI%2FsQEwftRprl%2FQjcE2bg%3D%3D&pageNo=1&numOfRows=10&startCreateDt='
+COVID_KEY_END = '&endCreateDt='
+COVID_KEY_ALL = '?serviceKey=MMq1VsRlz5qKvsdKDrvMavJB5rGdJOA8JGKgyojceXcL5tj6MJtzG21jN30ke9OOHZI%2FsQEwftRprl%2FQjcE2bg%3D%3D&pageNo=1&numOfRows=10&startCreateDt=20200120&endCreateDt='+TODAY+'&'
+COVID_CITY_URL = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson'
+COVID_GEN_AGE_URL = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19GenAgeCaseInfJson'
+COVID_STATUS_URL = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
+
+
+CITY = 0
+STATUS = 1
+GENAGE = 2
